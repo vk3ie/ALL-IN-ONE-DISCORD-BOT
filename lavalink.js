f1a9a644ac8412778b18e6186667379510f01e58
vk3ie <fabianfacebook745@gmail.com>
2025-07-06 11:41:27 +0200

    Add files via upload

---
+++ lavalink.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "5.39.63.207",
+      port:  8262,
+      secure: false
+    }
+};
